867b2bcc543058e1732900906e64c0ee1f221d4e
Diego Engelhard <diegoengelhard@Diegos-MacBook-Pro.local>
2023-05-25 19:06:54 -0600

    Add: Project structure

---
+++ temp-parcial2/src/main/java/com/bancrabs/tempparcial2/TempParcial2Application.java
@@ -0,0 +1,13 @@
+package com.bancrabs.tempparcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempParcial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempParcial2Application.class, args);
+	}
+
+}
---
+++ temp-parcial2/src/test/java/com/bancrabs/tempparcial2/TempParcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancrabs.tempparcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempParcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
