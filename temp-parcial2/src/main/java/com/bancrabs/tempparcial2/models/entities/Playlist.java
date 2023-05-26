da7c36ffaca115cc5e3a3a10c6c1ff30332cf814
Diego Engelhard <diegoengelhard@Diegos-MacBook-Pro.local>
2023-05-25 19:32:21 -0600

    Add: User entity & Playlist entity

---
+++ temp-parcial2/src/main/java/com/bancrabs/tempparcial2/models/entities/Playlist.java
@@ -0,0 +1,39 @@
+package com.bancrabs.tempparcial2.models.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "playlist")
+public class Playlist{ 
+
+  @Id
+  @Column(name = "code")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID code;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "description")
+  private String description;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_code", nullable = false)
+  private User user;
+}
---
+++ temp-parcial2/src/main/java/com/bancrabs/tempparcial2/models/entities/User.java
@@ -0,0 +1,40 @@
+package com.bancrabs.tempparcial2.models.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+  @Id
+  @Column(name = "code")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID code;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "password")
+  private String password;
+
+  public User(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+}
